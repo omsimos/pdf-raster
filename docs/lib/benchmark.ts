255016a46ce4a8e9c413db55832190f9555a5990
Josh Daniel Bañares <joshxfi.dev@gmail.com>
2026-03-08 14:01:38 +0800

    chore: update latest benchmark

--- docs/lib/benchmark.ts
+++ docs/lib/benchmark.ts
@@ -1,17 +1,17 @@
 export const benchmarkSummary = {
-  speedupRange: "4x-7x",
+  speedupRange: "4x-8x",
   metrics: [
     {
       label: "Multi-page",
-      ours: "1.90 ms",
-      napi: "11.73 ms",
-      nodeCanvas: "14.19 ms",
+      ours: "1.72 ms",
+      napi: "11.96 ms",
+      nodeCanvas: "14.37 ms",
     },
     {
       label: "Single-page",
-      ours: "1.35 ms",
-      napi: "5.78 ms",
-      nodeCanvas: "7.15 ms",
+      ours: "1.29 ms",
+      napi: "5.81 ms",
+      nodeCanvas: "7.25 ms",
     },
   ],
 };
